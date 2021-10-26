aeb25300f146f0c768f86ecdc25842e24343ed88
김기도 <echachca@naver.com>
2021-10-26 12:05:44 +0900

    [BE] feat : 회원가입 기능 (S05P31B101-43)

--- backend/src/main/java/com/spectrum/controller/UserController.java
+++ backend/src/main/java/com/spectrum/controller/UserController.java
@@ -23,10 +23,7 @@ public class UserController {
     @PostMapping("/regist")
     @ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
     @ApiResponses({
-            @ApiResponse(code = 201, message = "성공"),
-            @ApiResponse(code = 401, message = "인증 실패"),
-            @ApiResponse(code = 404, message = "사용자 없음"),
-            @ApiResponse(code = 500, message = "서버 오류")
+            @ApiResponse(code = 200, message = "성공"),
     })
     public ResponseEntity<UserResponse> register(
             @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo,
