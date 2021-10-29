c7d80c4b4349fef595c8491bebe9625fba31314c
김기도 <echachca@naver.com>
2021-10-29 11:28:56 +0900

    [BE] feat : 로그인 api (S05P31B101-44)

--- backend/src/main/java/com/spectrum/controller/UserController.java
+++ backend/src/main/java/com/spectrum/controller/UserController.java
@@ -76,7 +76,7 @@ public class UserController {
 //            JwtTokenUtil.getToken(userEmail);
             String token = jwtUtil.generateToken(userinfo);
             redisUtil.deleteData(token);
-            System.out.println(jwtUtil.getUsername(token));
+            System.out.println(jwtUtil.getUsername(token)); //토큰 > userID
             return ResponseEntity.ok(UserResponse.of(200, "Success",token));
         }
         return ResponseEntity.ok(UserResponse.of(403, "아이디/비밀번호가 일치하지 않습니다."));
