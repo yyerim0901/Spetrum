5e95b4f9763d83c3948daa9b4746d111fbf47617
김기도 <echachca@naver.com>
2021-10-29 15:14:20 +0900

    [BE] feat : CORS설정

---
+++ backend/src/main/java/com/spectrum/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.spectrum.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://k5b101.p.ssafy.io");
+    }
+}
