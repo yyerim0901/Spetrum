27ea2d0d9316d09b0d4f3407950157e066480f68
김예림 <bam03196@naver.com>
2021-11-01 11:19:57 +0900

    [BE] feat : 내 게시글(petsitter)(#S05P31B101-58)

--- backend/src/main/java/com/spectrum/common/response/PetSitterResponse.java
+++ backend/src/main/java/com/spectrum/common/response/PetSitterResponse.java
@@ -1,32 +1,21 @@
 package com.spectrum.common.response;
 
-import com.spectrum.service.PetSitterService;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiOperation;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Date;
 
 @Getter
 @Setter
 @ApiModel("PetSitterResponse")
 public class PetSitterResponse {
 
-    private String title;
-    private String content;
-    private String picture;
-    private Date created;
-    private Date updated;
+    private String message;
+    private Object data;
 
-    public PetSitterResponse(String title, String content, String picture, Date created, Date updated){
-        this.title = title;
-        this.content = content;
-        this.picture = picture;
-        this.created = created;
-        this.updated = updated;
+    public PetSitterResponse(String message, Object data){
+        this.message = message;
+        this.data = data;
     }
+    public PetSitterResponse(){}
 
 }
--- backend/src/main/java/com/spectrum/controller/PetSitterController.java
+++ backend/src/main/java/com/spectrum/controller/PetSitterController.java
@@ -1,7 +1,9 @@
 package com.spectrum.controller;
 
+import com.spectrum.common.jwt.JWTUtil;
 import com.spectrum.common.request.PetSitterPostReq;
 import com.spectrum.common.request.PetSitterUpdateReq;
+import com.spectrum.common.response.PetSitterResponse;
 import com.spectrum.entity.PetSitter;
 import com.spectrum.service.PetSitterService;
 import io.swagger.annotations.Api;
@@ -14,6 +16,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
+
 
 @Api(value = "펫시터 API", tags = {"PetSitter"})
 @RestController
@@ -22,7 +28,10 @@ public class PetSitterController {
 
     @Autowired
     private PetSitterService petSitterService;
-    
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @ApiOperation(
             value = "펫시터 게시글 작성",
             notes = "**제목, 내용, 위도, 경도** 입력 ~~작성자는 토큰 구현 이후 수정 예정~~ "
@@ -30,12 +39,13 @@ public class PetSitterController {
     @PostMapping
     private ResponseEntity<String> postPetsitter(
             @ApiParam(value = "게시글 작성", required = true) PetSitterPostReq petSitterPostRequest,
-            @RequestPart(value = "image", required = false) MultipartFile petSitterImage){
+            @RequestPart(value = "image", required = false) MultipartFile petSitterImage,
+            HttpServletRequest request){
 
-//        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
 //        String userid = jwtUtil.getUsername(token);
 
-        petSitterService.postPetSitter(petSitterPostRequest,petSitterImage);
+        petSitterService.postPetSitter(petSitterPostRequest,petSitterImage, token);
         return new ResponseEntity<>("post petsitter success", HttpStatus.OK);
     }
 
@@ -62,6 +72,21 @@ public class PetSitterController {
             ){
         petSitterService.deletePetSitter(petSitterId);
         return new ResponseEntity<>("delete success", HttpStatus.OK);
+    }
+
+    @ApiOperation(
+            value = "내 펫시터 리스트",
+            notes = "**토큰**을 이용하여 리스트 출력"
+    )
+    @GetMapping("/mylist")
+    private PetSitterResponse myPetsitterList(HttpServletRequest request){
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        List<PetSitter> list = petSitterService.myPetsitterList(token);
+
+        if(list == null || list.isEmpty()){
+            return new PetSitterResponse("나의 리스트가 존재하지 않습니다.",null);
+        }else return new PetSitterResponse("리스트 출력 완료",list);
     }
 
 }
