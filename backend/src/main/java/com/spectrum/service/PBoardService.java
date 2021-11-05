4bf1ea230cc9269c980f234e98841e46a8438d16
DESKTOP-KVCQHCD\multicampus <kangjunlucky@naver.com>
2021-11-05 15:49:44 +0900

    [BE] feat : pBoard 펫시터 사진 crud

--- backend/src/main/java/com/spectrum/service/PBoardService.java
+++ backend/src/main/java/com/spectrum/service/PBoardService.java
@@ -5,14 +5,15 @@ import com.spectrum.common.request.PBoardUpdateReq;
 import com.spectrum.entity.PBoard;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface PBoardService {
 
-    public void postPetSitter(PBoardPostReq petSitterPostRequest, MultipartFile postPicture, String token);
+    public void postPetSitter(PBoardPostReq petSitterPostRequest, MultipartFile postPicture, String token) throws IOException;
     public boolean checkWriterOfBoard(PBoardUpdateReq pBoardUpdateReq, String token);
-    public void updatePetSitter(PBoardUpdateReq petSitterUpdateReq, MultipartFile newPicture);
-    public void deletePetSitter(Long petSitterId);
+    public void updatePetSitter(PBoardUpdateReq petSitterUpdateReq, MultipartFile newPicture, String token) throws IOException;
+    public void deletePetSitter(Long petSitterId) throws IOException;
     public List<PBoard> myPetsitterList(String token);
     public List<PBoard> allPetsitterList();
     public PBoard detailOfPetsitter(Long petsitterId);
