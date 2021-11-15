2f77effa47b2f675d6d4b6f6dd1f58ecf4fabb31
DESKTOP-KVCQHCD\multicampus <kangjunlucky@naver.com>
2021-11-15 17:04:05 +0900

    [FE] refact : userid 보여주기

--- frontend/src/components/molecules/PetSitterDetail.vue
+++ frontend/src/components/molecules/PetSitterDetail.vue
@@ -4,7 +4,7 @@
         <div class="board-box" @click="moveEachDetail">
             <p>{{ board.title }}</p>
             <p>{{ board.created }}</p>
-            <p>{{ board }}</p>
+            <p>{{ board.user.userid }}</p>
         </div>
     </div>
 </template>
