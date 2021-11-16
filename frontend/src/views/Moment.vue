11f7152b1e767a3eb2ada35ec686e1a662fb15e4
moongchi98 <kwaknh98@gmail.com>
2021-11-17 01:15:16 +0900

    [FE] fix: moment

--- frontend/src/views/Moment.vue
+++ frontend/src/views/Moment.vue
@@ -33,7 +33,7 @@
 import Footer from '../components/molecules/Footer.vue'
 import Header from '../components/molecules/Header.vue'
 import StyledButton from '../components/atoms/StyledButton'
-import axios from 'axios'
+// import axios from 'axios'
 import {mapState} from 'vuex';
 export default {
   name:'Moment',
@@ -82,6 +82,7 @@ export default {
     getthumbnail(){
       if (this.userInfo.thumbnail) {
         var fullurl = this.BASE_URL + this.thumbnail
+        console.log(fullurl,'fullurl');
         return fullurl
       }else{
         return require("@/assets/img_logo.jpg")
@@ -121,36 +122,36 @@ export default {
     this.userid = localStorage.getItem('userid');
     // console.log(localStorage.getItem)
     // console.log(this.userid)
-    axios({
-        url:'https://spetrum.io:8080/api/sns/',
-        method:'get',
-        params:{
-          page:1,
-        },
-        headers: {
-          "Authorization": localStorage.getItem("token")
-        },
-      })
-      .then(res => {
-        console.log(res.data.data)
-        this.mywrites = res.data.data;
-        this.$store.dispatch ('requestUser',this.userid);
-      })
+    // axios({
+    //     url:'https://spetrum.io:8080/api/sns/',
+    //     method:'get',
+    //     params:{
+    //       page:1,
+    //     },
+    //     headers: {
+    //       "Authorization": localStorage.getItem("token")
+    //     },
+    //   })
+    //   .then(res => {
+    //     console.log(res.data.data)
+    //     this.mywrites = res.data.data;
+    //     this.$store.dispatch ('requestUser',this.userid);
+    //   })
     // this.$store.dispatch('bringSBoard',this.page)
     // .then(res=>{
     //   this.mywrites = res.data.data;
     // })
-    // this.$store.dispatch('requestUser',localStorage.getItem('userid'));
-    // this.$store.dispatch('bringSBoard',1)
-    // .then(res=>{
-    //   this.mywrites = res.data.data;
-    //   console.log('여기서에러')
-    //   console.log(res)
-    // })
-    // .catch(err=>{
-    //   console.log('나는 게시판 받아오는 에러');
-    //   console.log(err)
-    // })
+    this.$store.dispatch('requestUser',localStorage.getItem('userid'));
+    this.$store.dispatch('bringSBoard',this.page)
+    .then(res=>{
+      this.mywrites = res.data.data;
+      console.log('여기서에러')
+      console.log(res)
+    })
+    .catch(err=>{
+      console.log('나는 게시판 받아오는 에러');
+      console.log(err)
+    })
   },
 }
 </script>
