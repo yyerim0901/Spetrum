54b24f9db09a78fd67466eb59891805bcaa85ee8
김예림 <bam03196@naver.com>
2021-11-17 19:27:04 +0900

    [FE] refact : petsitter

--- frontend/src/views/PetSitter.vue
+++ frontend/src/views/PetSitter.vue
@@ -4,16 +4,16 @@
         <div class="sitter-box">
             <div class="sitter-box-in">
                 <div class="siiter-small-box" style="flex-direction: column;">
-                    <h3><span>우리동네&nbsp;겟!&nbsp;시터</span></h3>
+                    <h3><span>우리동네&nbsp;<span style="color:#F7797D;">겟!</span>&nbsp;시터</span></h3>
                     <span style="height:1px;"></span>
-                    <h3><i class="fas fa-map-marker-alt"></i><span>&nbsp;&nbsp;현재 나의 주소</span></h3>
+                    <h4><i class="fas fa-map-marker-alt"></i><span style="color:#636E72;">&nbsp;&nbsp;{{this.location}}</span></h4>
                 </div>
                 <div>
                 <StyledButton style="float:right;" bcolor="pink" btype="small" @click="moveAddSitter">글작성하기</StyledButton>          
                 </div>
             </div>
             <hr class="hr-box" style="text-align:start; border:solid; border-width:1px 0; border-color:#E5EAEF;">
-            <div class="sitter-box" style="overflow-y:scroll; margin-bottom:20px;">
+            <div class="sitter-box-in-second" style="margin-bottom:20px;">
                 <PetSitterDetail v-for="(board, idx) in Boards" :key="idx" :board="board" />
             </div>
         </div>
@@ -41,6 +41,7 @@ export default {
     data() {
         return{
             isActive: 3,
+            location:'',
         }
     },
     computed: {
@@ -54,39 +55,29 @@ export default {
             this.$router.push({name:'AddPetSitter'})
         },
         BringMyLocation(){
-            axios({
-                url:`https://dapi.kakao.com/v2/local/geo/coord2address.json`,
-                method:'get',
-                params:{
-                    x : this.MyLocation.lat,
-                    y : this.MyLocation.lng,
-                },
-                headers: {
-                "Authorization": `KakaoAk 129bac39970b463ea7ef94eff7029109`
-                },
-            }).then(res => {
-                console.log(res)
-            })
-            // var latlng = new kakao.maps.LatLng(37, 127);
-            // latlng.getLat(); // 37
-
-            // var geocoder = new kakao.maps.services.Geocoder();
-
-            // var coord = new kakao.maps.LatLng(37.56496830314491, 126.93990862062978);
-            // var callback = function(result, status) {
-            //     if (status === kakao.maps.services.Status.OK) {
-            //         console.log('그런 너를 마주칠까 ' + result[0].address.address_name + '을 못가');
-            //     }
-            // };
-
-            // geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-
-
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition((pos)=>{
+                    axios({
+                        url:`//dapi.kakao.com/v2/local/geo/coord2address.json`,
+                        method:'get',
+                        params:{
+                            x : pos.coords.longitude,
+                            y : pos.coords.latitude,
+                        },
+                        headers: {
+                        "Authorization": `KakaoAK 129bac39970b463ea7ef94eff7029109`
+                        },
+                    }).then(res => {
+                        console.log(res.data.documents[0].road_address.address_name)
+                        var loc = res.data.documents[0].road_address.address_name.split(" ");
+                        this.location = loc[0] + " " + loc[1]
+                    })
+                })
+            }
         }
     },
     created() {
         this.$store.dispatch('getBoards')
-        console.log(this.MyLocation.lat)
         this.BringMyLocation()
     }
 }
@@ -113,6 +104,12 @@ export default {
         width: 100%;
         margin-top: 8px;
         
+    }
+    .sitter-box-in-second{
+        width: 100%;
+        padding:0px 60px;
+        display: flex;
+        overflow-y: scroll;
     }
   
 </style=>
