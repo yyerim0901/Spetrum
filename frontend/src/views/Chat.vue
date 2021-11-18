5fd5ae6f0f26edbf447ec6dbcfa8fd879d6198b8
김기도 <echachca@naver.com>
2021-11-18 19:08:34 +0900

    [FE] style : 채팅전송버튼, 입력폼 위치변경

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -135,8 +135,8 @@ export default {
       })
     },
     connect() {
-      // const serverURL = "https://spetrum.io:8080/chat"
-      const serverURL = "http://localhost:8080/chat"
+      const serverURL = "https://spetrum.io:8080/chat"
+      // const serverURL = "http://localhost:8080/chat"
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
 
